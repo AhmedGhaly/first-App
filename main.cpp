d17455db6529139b773706ab97afd2894132de20
AhmedGhaly <ahmed_ghaly64@yahoo.com>
2017-05-14 17:14:24 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Abdo is donkey!!!" << endl;
+    cout << "ahmed ghaly and my brothers is abdo ghaly" << endl;
+    return 0;
+}
